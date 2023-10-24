461832aad572fd6685530277fef7b632145a6f74
darthmbare <lory97.b@gmail.com>
2023-10-24 21:13:24 +0200

    esercitazione 2

---
+++ esarcitazione-2/esercitazione1.js
@@ -0,0 +1,40 @@
+numero1 = 2;
+numero2 = 5;
+numero3 = 3;
+numero4 = 9;
+numero5 = 1;
+somma = numero1 + numero2 + numero3 + numero4 + numero5;
+media = somma / 5;
+console.log(`La somma trai numeri equivale a ${somma} e la media equivale a ${media}`);
+
+annocorrente = 2023
+annonascita = 1997
+età = annocorrente - annonascita;
+mancanza = 100 - età;
+console.log(`Hai ${età} anni e ti mancano ${mancanza} anni per compierne 100`);
+
+const variabile = parseInt(prompt('Inserisci un numero:'))
+const variabile2 = parseInt(prompt('Inserisci un numero:'))
+somma1 = variabile + variabile2;
+sottrazione = variabile - variabile2;
+moltiplicazione = variabile * variabile2;
+divisione= variabile / variabile2;
+potenza = variabile ** variabile2;
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono: somma ${somma1}, sottrazione ${sottrazione}, moltiplicazione ${moltiplicazione}, divisione ${divisione} e potenza ${potenza}`);
+
+
+let array_1 = [ 
+    ['un', 'per', 'incatenarli.'],
+    ['Anello', 'trovarli,'],
+    ['ghermirli', 'e'],
+    ['gondor', 'mark']
+    ];
+    let array_2 = [ 
+    [['trovarli,']],
+    ['tu,', 'sciocchi'],
+    ['tu,', 'sciocchi', ['padron', 'Sauron']],
+    ['nel', ['fuggite', 'gandalf']],
+    [['domarli,', 'passare'], 'buio']
+    ];
+
+    console.log(`${array_1 [0][0]} ${array_1[1][0]} ${array_1 [0][1]} ${array_2[4][0][0]} ${array_1[0][0]} ${array_1[1][0]} ${array_1[0][1]} ${array_1[1][1]} ${array_1[0][0]} ${array_1[0][0]} ${array_1[1][0]} ${array_1[2][0]} ${array_1[2][1]} ${array_2[3][0]} ${array_2[4][1]} ${array_1[0][2]}`)
